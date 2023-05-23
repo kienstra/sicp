981b62ac898bfd9b14dc9247dae78c0a7caf972b
Ryan Kienstra <kienstraryan@gmail.com>
2023-05-22 22:53:11 -0600

    Add solutions for exercise 1.31

--- src/sicp/sum.clj
+++ src/sicp/sum.clj
@@ -15,3 +15,22 @@
    0
    next
    b))
+
+(defn product-lin [term a next b]
+  (if (> a b)
+    1
+    (* (term a)
+       (product-lin term (next a) next b))))
+
+(defn product-iter [term a next b]
+  ((fn [a result next b]
+     (if (> a b)
+       result
+       (recur (next a) (* result (term a)) next b)))
+   a
+   1
+   next
+   b))
+
+(defn factorial [n]
+  (product-lin identity 1 inc n))
