6d2119fd944c86032bbc547582bf1c8e9977405e
Ryan Kienstra <kienstraryan@gmail.com>
2023-06-17 15:03:49 -0600

    Exercise 2.79: Add an equ? operation

--- src/sicp/generic.clj
+++ src/sicp/generic.clj
@@ -48,6 +48,8 @@
                   (fn [x y] (tag-scheme-number (* x y))))
   (put-operation! 'div '(scheme-number scheme-number)
                   (fn [x y] (tag-scheme-number (/ x y))))
+  (put-operation! 'equ? '(scheme-number scheme-number)
+                  (fn [x y] (= x y)))
   (put-operation! 'make 'scheme-number
                   (fn [x] (tag-scheme-number x)))
   'done)
@@ -80,6 +82,11 @@
   (make-rat (* (numer x) (denom y))
             (* (denom x) (numer y))))
 
+(defn equ-rat [x y]
+  (and
+   (= (numer x) (numer y))
+   (= (denom x) (denom y))))
+
 (defn tag-rat [x] (attach-tag 'rational x))
 (defn install-rational-package! []
   (put-operation! 'add '(rational rational)
@@ -90,6 +97,8 @@
                   (fn [x y] (tag-rat (mul-rat x y))))
   (put-operation! 'div '(rational rational)
                   (fn [x y] (tag-rat (div-rat x y))))
+  (put-operation! 'equ? '(rational rational)
+                  (fn [x y] (equ-rat x y)))
 
   (put-operation! 'make 'rational
                   (fn [n d] (tag-rat (make-rat n d))))
@@ -97,10 +106,10 @@
 (defn make-rational [n d]
   ((get-operation 'make 'rational) n d))
 
-(defn make-from-real-imag [x y]
-  ((get-operation 'make-from-real-imag 'rectangular) x y))
-(defn make-from-mag-ang [r a]
-  ((get-operation 'make-from-mag-ang 'polar) r a))
+(defn make-complex-from-real-imag [x y]
+  ((get-operation 'make-from-real-imag 'complex) x y))
+(defn make-complex-from-mag-ang [r a]
+  ((get-operation 'make-from-mag-ang 'complex) r a))
 (defn real-part [z] (first z))
 (defn imag-part [z] (second z))
 (defn square [x] (* x x))
@@ -109,18 +118,23 @@
 (defn angle [z]
   (Math/atan (real-part z))) ;; used to have a first argument of (imag-part z)
 (defn add-complex [z1 z2]
-  (make-from-real-imag (+ (real-part z1) (real-part z2))
-                       (+ (imag-part z1) (imag-part z2))))
+  (make-complex-from-real-imag (+ (real-part z1) (real-part z2))
+                               (+ (imag-part z1) (imag-part z2))))
 (defn sub-complex [z1 z2]
-  (make-from-real-imag (- (real-part z1) (real-part z2))
-                       (- (imag-part z1) (imag-part z2))))
+  (make-complex-from-real-imag (- (real-part z1) (real-part z2))
+                               (- (imag-part z1) (imag-part z2))))
 (defn mul-complex [z1 z2]
-  (make-from-mag-ang (* (magnitude z1) (magnitude z2))
-                     (+ (angle z1) (angle z2))))
+  (make-complex-from-mag-ang (* (magnitude z1) (magnitude z2))
+                             (+ (angle z1) (angle z2))))
 (defn div-complex [z1 z2]
-  (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
-                     (- (angle z1) (angle z2))))
+  (make-complex-from-mag-ang (/ (magnitude z1) (magnitude z2))
+                             (- (angle z1) (angle z2))))
+(defn equ-complex [z1 z2]
+  (and (= (real-part z1) (real-part z2))
+       (= (imag-part z1) (imag-part z2))))
 (defn tag-complex [z] (attach-tag 'complex z))
+(defn make-from-real-imag-polar [x y]
+  (list x y))
 
 (defn install-complex-package! []
   (put-operation! 'add '(complex complex)
@@ -131,18 +145,16 @@
                   (fn [z1 z2] (tag-complex (mul-complex z1 z2))))
   (put-operation! 'div '(complex complex)
                   (fn [z1 z2] (tag-complex (div-complex z1 z2))))
+  (put-operation! 'equ? '(complex complex)
+                  (fn [z1 z2] (equ-complex z1 z2)))
   (put-operation! 'make-from-real-imag 'complex
-                  (fn [x y] (tag-complex (make-from-real-imag x y))))
+                  (fn [x y] (tag-complex (make-from-real-imag-polar x y))))
   (put-operation! 'make-from-mag-ang 'complex
-                  (fn [r a] (tag-complex (make-from-mag-ang r a))))
+                  (fn [r a] (tag-complex (make-complex-from-mag-ang r a))))
   'done)
-
-(defn make-complex-from-real-imag [x y]
-  ((get 'make-from-real-imag 'complex) x y))
-(defn make-complex-from-mag-ang [r a]
-  ((get 'make-from-mag-ang 'complex) r a))
 
 (defn add [x y] (apply-generic 'add x y))
 (defn sub [x y] (apply-generic 'sub x y))
 (defn mul [x y] (apply-generic 'mul x y))
 (defn div [x y] (apply-generic 'div x y))
+(defn equ? [x y] (apply-generic 'equ? x y))
