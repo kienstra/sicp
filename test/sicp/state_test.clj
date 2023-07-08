58e6e40f2d89fcb86a463119e9b75a597c286b42
Ryan Kienstra <kienstraryan@gmail.com>
2023-07-08 16:33:19 -0600

    Exercise 3.23: Add a deque protocol

--- test/sicp/state_test.clj
+++ test/sicp/state_test.clj
@@ -2,10 +2,23 @@
   (:require [clojure.test :refer [deftest is testing]]
             [sicp.state :refer [->account
                                 balance
+                                delete-queue
                                 deposit
+                                ->deque
+                                empty-deque?
+                                empty-queue?
+                                front-delete-deque
+                                front-deque
+                                front-queue
+                                front-insert-deque
+                                insert-queue
                                 make-account
                                 make-accumulator
                                 make-monitored
+                                rear-delete-deque
+                                rear-deque
+                                rear-insert-deque
+                                ->queue
                                 withdraw]]
             [sicp.fixed-point :refer [sqrt]]))
 
@@ -35,4 +48,14 @@
   (testing "Account withdrawal"
     (is (= 0 (balance (withdraw (->account 100) 100))))
     (is (= 90 (balance (withdraw (->account 100) 10))))
-    (is (= {:error "Insufficient funds"} (withdraw (->account 100) 101)))))
+    (is (= {:error "Insufficient funds"} (withdraw (->account 100) 101))))
+  (testing "Queue"
+    (is (= false (empty-queue? (insert-queue (->queue '()) 9))))
+    (is (= true (empty-queue? (delete-queue (insert-queue (->queue '()) 9)))))
+    (is (= 5 (front-queue (insert-queue (->queue '()) 5)))))
+  (testing "Deque"
+    (is (= false (empty-deque? (front-insert-deque (->deque '()) 7))))
+    (is (= 7 (front-deque (front-insert-deque (->deque '()) 7))))
+    (is (= 19 (rear-deque (rear-insert-deque (->deque '()) 19))))
+    (is (empty-deque? (front-delete-deque (rear-insert-deque (->deque '()) 19))))
+    (is (empty-deque? (rear-delete-deque (rear-insert-deque (->deque '()) 19))))))
